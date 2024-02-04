4fc6b2aa96256e3edb268cfe5cb529bc2535e477
Noam <noamshvartsman@protonmail.com>
2024-02-04 09:15:39 +0000

    modified: .vscode/tasks.json new file: Program modified: Program.c

--- Program.c
+++ Program.c
@@ -1,4 +1,38 @@
 #include <stdio.h>
-int main(){
-    printf("Hello World!");
+#include <stdbool.h>
+#include <string.h>
+
+#define LENGTH 50
+
+void getInputAndCheck(char stringarr[]);
+bool checkForPolyndrome(char stringarr[]);
+
+int main()
+{
+    char str[LENGTH] = " ";
+    char strcopy[LENGTH] = " ";
+
+    getInputAndCheck(str);
+    checkForPolyndrome(str);
+}
+
+void getInputAndCheck(char stringarr[])
+{
+    printf("Please enter the input to check: ");
+    fgets(stringarr, LENGTH, stdin);
+}
+
+bool checkForPolyndrome(char stringarr[])
+{
+    char stringreversecopy[] = " ";
+    int i = 0;
+    int lengthOfCurrent = 0;
+
+    lengthOfCurrent = strlen(stringarr);
+    for (i = 0; i<lengthOfCurrent; i++)
+    {
+        stringreversecopy[i] = stringarr[lengthOfCurrent - i];
+    }
+
+    printf("%s", stringreversecopy);
 }
